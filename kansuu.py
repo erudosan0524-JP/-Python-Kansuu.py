aa09cdcc69cb80c2d813f246bc5e3e3dc2e47c5b
erudo <hashierudo@gmail.com>
2018-05-19 15:48:16 +0900

    Add files via upload

--- kansuu.py
+++ kansuu.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import QIcon
+from matplotlib.font_manager import FontProperties
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -21,15 +22,13 @@ class Kansuu(QWidget):
 
         self.radio1 = QRadioButton('一次関数')
         self.radio2 = QRadioButton('二次関数')
+        self.radio3 = QRadioButton('三次関数')
 
         self.group = QButtonGroup()
         self.group.addButton(self.radio1,1)
         self.group.addButton(self.radio2,2)
+        self.group.addButton(self.radio3,3)
         self.radio1.toggle()
-        
-        hbox0 = QHBoxLayout()
-        hbox0.addWidget(self.radio1)
-        hbox0.addWidget(self.radio2)
 
         lbl_a = QLabel('aの値')
         self.a = QLineEdit(self)
@@ -37,6 +36,13 @@ class Kansuu(QWidget):
         self.b = QLineEdit(self)
         lbl_c = QLabel('cの値')
         self.c = QLineEdit(self)
+        lbl_d = QLabel('dの値')
+        self.d = QLineEdit(self)
+
+        hbox0 = QHBoxLayout()
+        hbox0.addWidget(self.radio1)
+        hbox0.addWidget(self.radio2)
+        hbox0.addWidget(self.radio3)
 
         button = QPushButton("関数作成！", self)
         button.clicked.connect(self.buttonClicked)
@@ -48,6 +54,8 @@ class Kansuu(QWidget):
         hbox.addWidget(self.b)
         hbox.addWidget(lbl_c)
         hbox.addWidget(self.c)
+        hbox.addWidget(lbl_d)
+        hbox.addWidget(self.d)
         
         vbox = QVBoxLayout()
         vbox.addLayout(hbox0)
@@ -58,28 +66,43 @@ class Kansuu(QWidget):
         self.show()
 
     def buttonClicked(self):
-        if self.radio1.isChecked()  == True:
-            intA1 = int(self.a.text())
-            intC1 = int(self.c.text())
-            x1 = np.arange(0,10,0.1)
-            y1 = intA1*x1 + intC1
-            plt.scatter(x1,y1, label="y = " + str(intA1) + "x + " + str(intC1))
-            plt.legend()
-            plt.plot(x1,y1)
-            plt.show()
 
+        fp = FontProperties(fname=r'C:\WINDOWS\Fonts\HGRSMP.TTF',size=14)
+
+        if self.radio1.isChecked()  == True:
+            intA = int(self.a.text())
+            intC = int(self.c.text())
+            x = np.arange(-10,10,0.1)
+            y = intA*x + intC
+            plt.title(u'一次関数のグラフ', fontproperties=fp)
+            plt.plot(x,y, label="y=" + str(intA) + "x+" + str(intC))
+            plt.legend()
+            plt.show()
 
         elif self.radio2.isChecked() == True:
-            intA2 = int(self.a.text())
-            intB2 = int(self.b.text())
-            intC2 = int(self.c.text())
-            x2 = np.arange(0,10,0.1)
-            y2 = intA2*x2**2 + intB2*x2 + intC2
-            plt.scatter(x2,y2, label="y = " + str(intA2) + "x^2 + " + str(intB2) + "x + " + str(intC2))
+            intA = int(self.a.text())
+            intB = int(self.b.text())
+            intC = int(self.c.text())
+            x = np.arange(-10,10,0.1)
+            y = intA*x**2 + intB*x + intC
+            plt.title(u'二次関数のグラフ', fontproperties=fp)
+            plt.plot(x,y, label="y=" + str(intA) + "x^2+" + str(intB) + "x+" + str(intC))
             plt.legend()
-            plt.plot(x2,y2)
             plt.show()
-            
+
+        elif self.radio3.isChecked() == True:
+            intA = int(self.a.text())
+            intB = int(self.b.text())
+            intC = int(self.c.text())
+            intD = int(self.d.text())
+            x = np.arange(-10,10,0.1)
+            y = intA*x**3 + intB*x**2 + intC*x + intD
+            plt.title(u'三次関数のグラフ', fontproperties=fp)
+            plt.plot(x,y, label="y=" + str(intA) + "x^3+" + str(intB) + "x^2+" + str(intC) + "x+" + str(intD))
+            plt.legend()
+            plt.show()
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     kansuu = Kansuu()
